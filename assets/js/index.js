d0a66476e1664f15218f89c2563ac8936a32c904
23birola <23birola@gmail.com>
2024-01-07 12:42:26 +0000

    add logit for localStorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,24 @@
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 const currentDay = dayjs().format('dddd, MMMM Do');
-const currentTime = parseInt(dayjs().format('h'));
+const currentTime = parseInt(dayjs().format('H'));
 $('#currentDay').text(currentDay);
 
-console.log(typeof(currentTime));
-
-const hoursArr = [9, 10, 8, 12, 13, 14, 15, 16, 17];
+const hoursArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+const savedTasks = localStorage;
+const taskkeys = Object.keys(savedTasks);
 
 const renderRows = (hours) => {
-  const container = $('.container.time-block');
+const container = $('.container.time-block');
   hours.map(hour => {
     const row = $("<div class='row'>");
-    const hr = $("<div class='hour col-2'>").text(hour.toString());
+    const currentHr = dayjs().hour(hour).format('hA');
+    const hr = $("<div class='hour col-2'>").text(currentHr);
     row.append(hr);
-    const textArea = $("<div class='col-8'>");
+    const textArea = $("<textarea class='col-8'>");
     handleColor(hour, currentTime, textArea);
+    if (taskkeys.includes(currentHr)) {
+      textArea.text(savedTasks[currentHr]);
+    }
     row.append(textArea);
     const btn = $("<button class='saveBtn col-2'>").text('Save');
     row.append(btn);
@@ -22,13 +26,27 @@ const renderRows = (hours) => {
   })
 }
 
-renderRows(hoursArr);
 function handleColor (time, current, e) {
   if (time < current) {
     e.addClass('past');
+    e.prop('disabled', true);
+
   } else if (time === current) {
     e.addClass('present');
+    e.prop('disabled', false);
   } else {
     e.addClass('future');
+    e.prop('disabled', false);
   }
 }
+
+const handleSaveBtn = function (e) {
+  e.preventDefault();
+  const task = $(e.target);
+  const description = task.parent().find('textarea').val().trim();
+  const taskHr = task.parent().find('.hour').text();
+  description && localStorage.setItem(taskHr, description);  
+}
+
+renderRows(hoursArr);
+$('.time-block').on('click', '.saveBtn', handleSaveBtn);
